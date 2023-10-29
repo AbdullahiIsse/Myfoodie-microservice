fa14da436c6429e17893843870a51cfa8fdc03d3
abdullahi <abdullahi12351@gmail.com>
2023-10-29 15:37:26 +0100

    Added the new service, favorite Dish Service to the microservice

---
+++ favorite-dish-service/src/test/java/com/myfoodie/favoritedishservice/FavoriteDishServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfoodie.favoritedishservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteDishServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
