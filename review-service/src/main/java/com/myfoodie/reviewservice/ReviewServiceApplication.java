dbd951c621adc3336574ba62930e388abc722f2e
abdullahi <abdullahi12351@gmail.com>
2023-11-01 03:13:37 +0100

    Added swagger ui to the review service

--- review-service/src/main/java/com/myfoodie/reviewservice/ReviewServiceApplication.java
+++ review-service/src/main/java/com/myfoodie/reviewservice/ReviewServiceApplication.java
@@ -1,11 +1,17 @@
 package com.myfoodie.reviewservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "MyFoodie - Review-Service Api"))
+
 public class ReviewServiceApplication {
 
     public static void main(String[] args) {
