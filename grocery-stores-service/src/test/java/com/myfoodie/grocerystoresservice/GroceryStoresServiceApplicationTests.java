72d6401d251ea7a24c19a82ac86e3e7516db164a
abdullahi <abdullahi12351@gmail.com>
2023-11-03 06:30:25 +0100

    added the Grocery-Stores-Service to the microservice

---
+++ grocery-stores-service/src/test/java/com/myfoodie/grocerystoresservice/GroceryStoresServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfoodie.grocerystoresservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoresServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
