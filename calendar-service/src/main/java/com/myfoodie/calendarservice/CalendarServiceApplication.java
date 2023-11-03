d38e970ce6ed4ac1b1c73d9f637ba6d27aed7615
abdullahi <abdullahi12351@gmail.com>
2023-11-03 06:20:04 +0100

    added the swagger ui to the calendar service

--- calendar-service/src/main/java/com/myfoodie/calendarservice/CalendarServiceApplication.java
+++ calendar-service/src/main/java/com/myfoodie/calendarservice/CalendarServiceApplication.java
@@ -1,11 +1,17 @@
 package com.myfoodie.calendarservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "MyFoodie - Calender-Service Api"))
+
 public class CalendarServiceApplication {
 
     public static void main(String[] args) {
